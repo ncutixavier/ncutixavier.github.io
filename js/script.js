ca4310fd9eede051a3904cf30d03dbec29f6fd4c
ncutixavier <ncuti65@gmail.com>
2020-07-26 20:35:31 +0200

    Updating whole website

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 window.onscroll = function () {
-    if (document.body.scrollTop >= 40 || document.documentElement.scrollTop >= 40) {
+    if (document.body.scrollTop >= 300 || document.documentElement.scrollTop >= 300) {
         document.querySelector('.navbar').classList.add("white-background")
+        document.querySelector('.goUpBtn').style.display = "block"
     }else{
         document.querySelector('.navbar').classList.remove("white-background")
+        document.querySelector('.goUpBtn').style.display = "none"
     }
 };
